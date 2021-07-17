769b2f01b29054e6c809e8324ee4fbb641d28c46
Roberto Barbosa <robertojf@gmail.com>
2021-07-17 14:12:48 -0300

    Fix lint

--- src/controllers/frontend/angular/table/table.template.ts
+++ src/controllers/frontend/angular/table/table.template.ts
@@ -3,7 +3,7 @@ import { TableAngular } from '../table';
 export class TableTemplate {
   tableTemplate = new TableAngular();
 
-  setRow = () => {};
+  setRow() {}
 
-  setColumn = () => {};
+  setColumn() {}
 }
