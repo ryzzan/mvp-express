ae33d77d524001fcffd35888d8cf29777237a645
Roberto Barbosa <robertojf@gmail.com>
2021-07-17 23:45:59 -0300

    Remove pure

--- src/controllers/frontend/pure.ts
+++ src/controllers/frontend/pure.ts
@@ -2,18 +2,18 @@ import { BuildedCode, ObjectToCode } from '../../../interfaces/frontend';
 import { FormPure } from './pure/form';
 
 export class CodeToPure {
-  formPure = new FormPure();
+  // formPure = new FormPure();
 
   setPureCode(objectToCode: ObjectToCode): BuildedCode {
-    let codeHtml = '';
-    let codeDirective = '';
-    let codeInterface = '';
+    const codeHtml = '';
+    const codeDirective = '';
+    const codeInterface = '';
 
-    if (objectToCode.form) {
-      codeHtml += this.formPure.setFormHtml(objectToCode.form);
-      codeDirective += this.formPure.setFormDirective(objectToCode.form);
-      codeInterface += this.formPure.setFormInterface(objectToCode.form);
-    }
+    // if (objectToCode.form) {
+    //   codeHtml += this.formPure.setFormHtml(objectToCode.form);
+    //   codeDirective += this.formPure.setFormDirective(objectToCode.form);
+    //   codeInterface += this.formPure.setFormInterface(objectToCode.form);
+    // }
 
     return {
       template: codeHtml,
